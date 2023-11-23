7dacd6a26ccf8a379ec061af866c2096d58b8824
EC2 Default User <ec2-user@ip-172-31-14-204.ec2.internal>
2023-11-23 01:17:07 +0000

    22-nov-2023 - lambda

---
+++ lambda_Register/lambda.py
@@ -0,0 +1,17 @@
+#read csv file
+
+import csv
+import json
+
+def readcsv(file):
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            print(row)
+            
+def lambda_handler(event, context):
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
